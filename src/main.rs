94d732791cedd3b10647f76afb486f5429369304
Wesley Ellis <tahnok@gmail.com>
2023-06-11 12:18:53 -0400

    send to influx in thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+use anyhow::Result;
 use chrono::{DateTime, Utc};
-use influxdb::Client;
 use influxdb::InfluxDbWriteable;
 use rumqttc::{AsyncClient, Event, MqttOptions, Packet, QoS};
 use serde::Deserialize;
-use serde_json::Error;
 use std::env;
 use std::fs::File;
 use std::time::Duration;
@@ -20,7 +19,7 @@ struct AQIReading {
     time: DateTime<Utc>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct StatenConfig {
     mqtt_url: String,
     mqtt_topic: String,
@@ -28,48 +27,57 @@ struct StatenConfig {
     influx_db: String,
 }
 
+async fn handle_publish(config: StatenConfig, event: rumqttc::Publish) -> () {
+    let influx_client = influxdb::Client::new(&config.influx_url, &config.influx_db);
+    let Ok::<AQIPacket, serde_json::Error>(packet) = serde_json::from_slice(&event.payload) else {
+        println!("error parsing packet");
+        return ();
+    };
+
+    println!("quality: {}", packet.pm25);
+
+    let to_write = AQIReading {
+        time: Utc::now(),
+        pm25: packet.pm25,
+    }
+    .into_query("aqi");
+    let write_result = influx_client.query(to_write).await;
+    if write_result.is_err() {
+        println!("error writing to influxdb");
+    }
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let config_path = match args.len() {
         1 => "/etc/staten_config.json",
         2 => &args[1],
         _ => panic!("Expected one optional argument of config path"),
     };
-    let config_file = File::open(config_path).unwrap();
-    let config: StatenConfig = serde_json::from_reader(config_file).unwrap();
+    let config_file = File::open(config_path)?;
+    let config: StatenConfig = serde_json::from_reader(config_file)?;
 
-    let mut mqttoptions = MqttOptions::parse_url(config.mqtt_url).unwrap();
+    let mut mqttoptions = MqttOptions::parse_url(config.mqtt_url.clone())?;
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
     let (mqtt_client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
     mqtt_client
-        .subscribe(config.mqtt_topic, QoS::AtMostOnce)
-        .await
-        .unwrap();
+        .subscribe(config.mqtt_topic.clone(), QoS::AtMostOnce)
+        .await?;
 
-    let influx_client = Client::new(config.influx_url, config.influx_db);
-
-    while let Ok(notification) = eventloop.poll().await {
-        match notification {
-            Event::Incoming(Packet::Publish(event)) => {
-                let parse: Result<AQIPacket, Error> = serde_json::from_slice(&event.payload);
-                if parse.is_ok() {
-                    let packet = parse.unwrap();
-                    println!("quality: {}", packet.pm25);
-
-                    let to_write = AQIReading {
-                        time: Utc::now(),
-                        pm25: packet.pm25,
-                    }
-                    .into_query("aqi");
-                    let write_result = influx_client.query(to_write).await;
-                    if write_result.is_err() {
-                        println!("error writing to influxdb");
-                    }
-                }
+    loop {
+        let event = eventloop.poll().await;
+        match event {
+            Ok(Event::Incoming(Packet::Publish(event))) => {
+                let config_clone: StatenConfig = config.clone();
+                tokio::spawn(async { handle_publish(config_clone, event).await });
+                ()
             }
-            _ => (),
+            Ok(_) => (),
+            Err(e) => {
+                println!("mqtt poll error: {}", e)
+            }
         }
     }
 }
